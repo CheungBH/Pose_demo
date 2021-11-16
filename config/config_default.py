a401c7af4bb8b2f3df02b79e2eb279062256529d
cheungbh <534660436@qq.com>
2021-11-16 17:29:14 +0800

    add config of json

--- config/config_default.py
+++ config/config_default.py
@@ -1,16 +1,18 @@
 
 
 detector_cfg = "/home/hkuit164/Downloads/nanodet_weights/coco/pytorch/nanodet-coco.yml"
-detector_weight = "/home/hkuit164/Downloads/pytorch_model_samples/mob3/pytorch/3_best_acc.pth"
+detector_weight = "/home/hkuit164/Downloads/nanodet_weights/coco/pytorch/model_last.pth"
 
 pose_weight = "/home/hkuit164/Downloads/pytorch_model_samples/mob3/pytorch/3_best_acc.pth"
 pose_model_cfg = ""
 pose_data_cfg = ""
 
-input_src = "/media/hkuit164/Elements/data/posetrack18/images/test/000691_mpii_test/000025.jpg"
+input_src = 0
 output_src = ""
 out_size = (1080, 720)
 show_size = (1080, 720)
 show = True
 
 write_json = True
+json_path = ""
+filter_criterion = {}
